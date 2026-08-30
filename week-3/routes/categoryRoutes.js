9c8e7375692f14fe44594b3ab536aa89eb5ecc77
shivambangar0006-tech <shivambangar0006@gmail.com>
2026-08-30 17:51:26 +0530

    Add files via upload

--- week-3/routes/categoryRoutes.js
+++ week-3/routes/categoryRoutes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const controller = require("../controllers/categoryController");
 
+const router = express.Router();
+
+router.get("/", controller.getCategories);
+router.post("/", controller.createCategory);
+
+module.exports = router;
