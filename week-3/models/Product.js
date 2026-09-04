d88d1792046428b035853dada1acf5b1ba2aebaa
shivambangar0006-tech <shivambangar0006@gmail.com>
2026-09-04 14:29:19 +0530

    Add Product model with schema definition

--- week-3/models/Product.js
+++ week-3/models/Product.js
@@ -1,1 +1,57 @@
 
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true
+    },
+
+    size: {
+      type: String,
+      trim: true
+    },
+
+    color: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    image: {
+      type: String,
+      trim: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
