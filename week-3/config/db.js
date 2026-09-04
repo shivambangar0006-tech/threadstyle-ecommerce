090d327eed81d38b292e5856145c88589d7525fc
shivambangar0006-tech <shivambangar0006@gmail.com>
2026-09-04 14:27:15 +0530

    Add MongoDB connection function using mongoose

--- week-3/config/db.js
+++ week-3/config/db.js
@@ -1,1 +1,15 @@
 
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    throw error;
+  }
+};
+
+module.exports = connectDB;
